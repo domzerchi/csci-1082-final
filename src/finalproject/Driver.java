4ae99bad34fd5d23baa150b63c4be550a46cb626
The-Ninya <55476384+The-Ninya@users.noreply.github.com>
2019-11-03 18:06:30 -0600

    Driver.java
    A driver to test methods in the Item class

---
+++ src/finalproject/Driver.java
@@ -0,0 +1,16 @@
+package finalproject;
+
+public class Driver {
+	public static void main(String[] args) {
+	
+		Item gso = new Item("Poecilotheria Metallica");
+		gso.setNote("This Old World tarantula is the only known blue species of the Poecilotheria genus.");
+		
+		System.out.println(gso.toString());
+		
+		gso.setType("Tarantula");
+		
+		System.out.println(gso.toString());
+		
+	}
+}
