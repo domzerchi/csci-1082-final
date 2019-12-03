027409f1b71c0248b82530038297b52a14375530
The-Ninya <55476384+The-Ninya@users.noreply.github.com>
2019-12-02 23:01:30 -0600

    Update Item.java
    edited deleteTag method - currently it leaves the array unsorted

--- src/finalproject/Item.java
+++ src/finalproject/Item.java
@@ -43,7 +43,14 @@ public class Item {
 		tags[numberOfTags++] = tag;
 	}
 	public void deleteTag(Tag tag) {
-		// TODO method Stub
+		for(int i = 0; i < tag.length; i++) {
+			if(tags[i] != null) {
+				if(tags[i].equals(tag)) {
+					tags[i] == null;
+					// sort tags
+				}
+			}
+		}
 	}
 
 	/**
