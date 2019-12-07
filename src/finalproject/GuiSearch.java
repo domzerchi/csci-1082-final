aafcb5af403b4a73b58804a2407311bd0769ccf4
The-Ninya <55476384+The-Ninya@users.noreply.github.com>
2019-12-07 15:53:35 -0600

    Update GuiSearch.java
    Made it so the panels will resize with the window. Colors/borderlines/actionListeners aren't finished.

--- src/finalproject/GuiSearch.java
+++ src/finalproject/GuiSearch.java
@@ -5,31 +5,31 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.border.LineBorder;
-import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import java.awt.GridLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
+import javax.swing.SwingConstants;
 
 public class GuiSearch {
 
 	private JFrame frame;
-	private JPanel viewPanel;
-	private JPanel settingsPanel;
-	private JPanel editPanel;
-	private JPanel tagPanel;
-	private JPanel toggleSearch;
-	private JPanel toggleImage;
-	private JLabel lblSearchBy;
-	private JCheckBox chckbxName;
-	private JCheckBox chkbxTag;
-	private JTextField txtSearch;
-	private JCheckBox chckbxType;
-	private JCheckBox chckbxNewCheckBox;
-	private JPanel filePanel;
-	private JButton newBtn;
-	private JButton openBtn;
+	private JPanel DisplayPanel;
+	private JPanel editPnl;
+	private JLabel titleLbl;
+	private JTextField searchFld;
+	private JPanel toggleSearchPnl;
+	private JLabel searchByLbl;
+	private JCheckBox nameChkbx;
+	private JCheckBox tagChkbx;
+	private JCheckBox toggleImgChkbx;
+	private JPanel searchPnl;
+	private JPanel tagPnl;
+	private JTextField addTagFld;
+	private JPanel addedTagsPnl;
+	private JPanel filePnl;
+	private JButton newClxnBtn;
+	private JButton openClxnBtn;
 
 	/**
 	 * Create the application.
@@ -44,76 +44,64 @@ public class GuiSearch {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
-		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		viewPanel = new JPanel();
-		viewPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		viewPanel.setBackground(Color.WHITE);
-		viewPanel.setBounds(0, 0, 400, 561);
-		frame.getContentPane().add(viewPanel);
+		editPnl = new JPanel();
+		frame.getContentPane().add(editPnl, BorderLayout.EAST);
+		editPnl.setLayout(new BorderLayout(0, 0));
 		
-		settingsPanel = new JPanel();
-		settingsPanel.setBorder(null);
-		settingsPanel.setBackground(Color.WHITE);
-		settingsPanel.setBounds(399, 0, 185, 561);
-		frame.getContentPane().add(settingsPanel);
-		settingsPanel.setLayout(new BorderLayout(0, 0));
+		searchPnl = new JPanel();
+		editPnl.add(searchPnl, BorderLayout.NORTH);
+		searchPnl.setLayout(new GridLayout(4, 0, 0, 0));
 		
-		editPanel = new JPanel();
-		settingsPanel.add(editPanel, BorderLayout.NORTH);
-		editPanel.setLayout(new GridLayout(3, 0, 0, 0));
+		titleLbl = new JLabel("*database*");
+		titleLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		searchPnl.add(titleLbl);
 		
-		toggleImage = new JPanel();
-		toggleImage.setBorder(new LineBorder(new Color(0, 0, 0)));
-		toggleImage.setBackground(Color.WHITE);
-		editPanel.add(toggleImage);
+		searchFld = new JTextField();
+		searchPnl.add(searchFld);
+		searchFld.setColumns(10);
 		
-		chckbxNewCheckBox = new JCheckBox("Toggle Images");
-		chckbxNewCheckBox.setBackground(Color.WHITE);
-		toggleImage.add(chckbxNewCheckBox);
+		toggleSearchPnl = new JPanel();
+		searchPnl.add(toggleSearchPnl);
 		
-		toggleSearch = new JPanel();
-		toggleSearch.setBorder(null);
-		toggleSearch.setBackground(Color.WHITE);
-		editPanel.add(toggleSearch);
-		toggleSearch.setLayout(new FlowLayout(FlowLayout.LEFT));
+		searchByLbl = new JLabel("Search by:");
+		toggleSearchPnl.add(searchByLbl);
 		
-		lblSearchBy = new JLabel("Search by:");
-		toggleSearch.add(lblSearchBy);
+		nameChkbx = new JCheckBox("Name");
+		toggleSearchPnl.add(nameChkbx);
 		
-		chckbxName = new JCheckBox("Name");
-		chckbxName.setBackground(Color.WHITE);
-		toggleSearch.add(chckbxName);
+		tagChkbx = new JCheckBox("Tag");
+		toggleSearchPnl.add(tagChkbx);
 		
-		chkbxTag = new JCheckBox("Tag");
-		chkbxTag.setBackground(Color.WHITE);
-		toggleSearch.add(chkbxTag);
+		toggleImgChkbx = new JCheckBox("Show Images");
+		searchPnl.add(toggleImgChkbx);
 		
-		chckbxType = new JCheckBox("Type");
-		toggleSearch.add(chckbxType);
+		tagPnl = new JPanel();
+		editPnl.add(tagPnl, BorderLayout.CENTER);
+		tagPnl.setLayout(new BorderLayout(0, 0));
 		
-		txtSearch = new JTextField();
-		editPanel.add(txtSearch);
-		txtSearch.setForeground(Color.LIGHT_GRAY);
-		txtSearch.setText("Search");
-		txtSearch.setColumns(18);
+		addTagFld = new JTextField();
+		tagPnl.add(addTagFld, BorderLayout.NORTH);
+		addTagFld.setColumns(10);
 		
-		tagPanel = new JPanel();
-		tagPanel.setBorder(null);
-		tagPanel.setBackground(Color.WHITE);
-		settingsPanel.add(tagPanel, BorderLayout.CENTER);
+		addedTagsPnl = new JPanel();
+		tagPnl.add(addedTagsPnl);
 		
-		filePanel = new JPanel();
-		filePanel.setBackground(Color.WHITE);
-		settingsPanel.add(filePanel, BorderLayout.SOUTH);
-		filePanel.setLayout(new GridLayout(0, 1, 0, 0));
+		filePnl = new JPanel();
+		editPnl.add(filePnl, BorderLayout.SOUTH);
+		filePnl.setLayout(new GridLayout(2, 0, 0, 0));
 		
-		newBtn = new JButton("New Database");
-		filePanel.add(newBtn);
+		newClxnBtn = new JButton("Create New Database");
+		filePnl.add(newClxnBtn);
 		
-		openBtn = new JButton("Open Database");
-		filePanel.add(openBtn);
-		frame.setBounds(600, 600, 600, 600);
+		openClxnBtn = new JButton("Open Existing Database");
+		filePnl.add(openClxnBtn);
+		
+		DisplayPanel = new JPanel();
+		DisplayPanel.setBackground(Color.WHITE);
+		frame.getContentPane().add(DisplayPanel, BorderLayout.CENTER);
+		frame.setBounds(0, 0, 600, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
