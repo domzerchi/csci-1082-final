2c6ffb6b839f2d29c9db6ef6c9761fbd3acf75e5
The-Ninya <55476384+The-Ninya@users.noreply.github.com>
2019-12-12 23:26:56 -0600

    Update Tag.java
    changed equals method along with variable name changes and other miscellaneous edits.

--- src/finalproject/Tag.java
+++ src/finalproject/Tag.java
@@ -1,72 +1,50 @@
 package finalproject;
 
 import java.util.Comparator;
-//All classes within Collection need to be serializable in order for reading/writing of databases to work
-import java.io.Serializable;
 
-/**
- * @author stephenpolson, aydensinn
- *
- */
-//All classes within Collection need to be serializable in order for reading/writing of databases to work
-public class Tag implements Serializable {
+public class Tag {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5547900002365413271L;
-	private String value;
+	private String name;
 
 	// constructor for a tag
-	public Tag(String value) {
+	public Tag(String name) {
 		super();
-		this.value = value;
+		this.name = name;
 	}
 
-	// accessor for value
-	public String getValue() {
-		return value;
+	public String getTag() {
+		return name;
 	}
-	// mutator for value
-	public void setValue(String value) {
-		this.value = value;
+	public void setTag(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || !(obj instanceof Tag))
+			return false;
+		Tag tag = (Tag) obj;
+		boolean isEqual = this.getTag().equals(tag.getTag());
+		return isEqual;
+	}
+
+	@Override
+	public String toString() {
+		return getTag();
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
-
-	// tests whether or not two values are the same
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null || getClass() != obj.getClass())
-			return false;
-		Tag other = (Tag) obj;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
-	}
-
-	// returns value with a hash in front of it
-	@Override
-	public String toString() {
-		return " #" + value;
-	}
-
-	public static Comparator<Tag> CompareByValue = new Comparator<Tag>() {
-
+	
+	public static Comparator<Tag> CompareByTag = new Comparator<Tag>() {
 		@Override
 		public int compare(Tag o1, Tag o2) {
-			return o1.getValue().compareTo(o2.getValue());
+			return o1.getTag().compareToIgnoreCase(o2.getTag());
 		}
 	};
 
